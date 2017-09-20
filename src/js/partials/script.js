43610de891af7abbadbf77be678d53d8131b3f79
bogdankhomiak <peculiarsev@gmail.com>
2017-09-20 18:37:22 +0300

    slider init

--- src/js/partials/script.js
+++ src/js/partials/script.js
@@ -1,6 +1,16 @@
 "use strict";
 $(document).ready(function () {
-    $('.slider-js').owlCarousel({
-        items: 1
+    $('.owl-carousel').owlCarousel({
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
     });
 });
