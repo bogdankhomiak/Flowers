d9ee4ecbce6df1eb61596419774bec37de432477
bogdankhomiak <peculiarsev@gmail.com>
2017-09-21 15:35:46 +0300

    our-flowers

--- src/js/partials/script.js
+++ src/js/partials/script.js
@@ -5,11 +5,19 @@ $(document).ready(function () {
     });
     var owl = $('.owl-carousel');
     owl.owlCarousel();
-// Go to the next item
     $('.slider-right-js').click(function () {
         owl.trigger('next.owl.carousel');
     });
     $('.slider-left-js').click(function () {
         owl.trigger('prev.owl.carousel');
-    })
+    });
+    $(function() {
+
+        $('.tabs__items').on('click', 'li:not(.active)', function() {
+            $(this)
+                .addClass('active').siblings().removeClass('active')
+                .closest('.tabs').find('.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+
+    });
 });
